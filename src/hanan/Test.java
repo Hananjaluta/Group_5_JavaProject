1feafd9125f556e1b86ae9506512a80f213eb600
Hanan Jaluta <hanan.k.jaluta@gmail.com>
2025-04-22 18:27:56 -0400

    test

--- src/hanan/Test.java
+++ src/hanan/Test.java
@@ -2,5 +2,7 @@ package hanan;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello everyone , project started");
+
+        System.out.println("test 2");
     }
 }
