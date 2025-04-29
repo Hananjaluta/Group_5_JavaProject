d4f3cba7999ca72e8c652fe50435dd18096588de
Muhammad <k.saim0000@gmail.com>
2025-04-29 15:27:59 -0400

    Odd Even task completed

--- src/saim/Question_1.java
+++ src/saim/Question_1.java
@@ -4,6 +4,9 @@ public class Question_1 {
 
     public static void main(String[] args) {
 
+        System.out.println(identifyNum(5));
+        System.out.println(identifyNum(6));
+
 
         /**
         Write  a method which can identify given number is even or odd
@@ -11,7 +14,17 @@ public class Question_1 {
          identify(5); ->"Odd"
          identify(6); ->"Even"
          */
+    }
+    public static String identifyNum(int n){
+
+        if(n %2 ==0){
+            return "Even";
+        }else
+            return "Odd";
 
 
     }
+
+
+
 }
