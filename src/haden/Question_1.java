0719819a31bc224d2af4746ddc40480d6e32414e
Haden <haden@cydeo.com>
2025-07-08 18:08:39 +0300

    New Questions added and solutions for 7 questions published

--- src/haden/Question_1.java
+++ src/haden/Question_1.java
@@ -1,15 +1,34 @@
 package haden;
 
+import java.util.Scanner;
+
 public class Question_1 {
 
     public static void main(String[] args) {
 
 
         /**
-        Write  a method which can identify given number is even or odd
+         Write  a method which can identify given number is even or odd
          Output ex:
          identify(5); ->"Odd"
          identify(6); ->"Even"
          */
+
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter a Whole Number");
+        int number = scn.nextInt();
+
+        identify(number);
+
     }
+    private static void identify(int i) {
+
+        if(i % 2 == 0) {
+            System.out.println(i + " is Even Number");
+        }else {
+            System.out.println(i + " is Odd Number");
+        }
+
+    }
+
 }
